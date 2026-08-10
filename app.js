d0f630ed0cb3fb65158b8c8a3a0be4daf8e6d639
xatoss <xatimatrix@gmail.com>
2026-08-10 23:31:54 +0200

    Add: weather API completed

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const city = document.getElementById("#city");
-const btSearch = document.getElementById("#btSearch");
-btSearch.addEventListener("click", searchWeather);
+const MY_Api_KEY = "b192813590df15f9733b4ad1755430c1";
+const theCity = document.querySelector("#city");
+const btSearch = document.querySelector("#btSearch");
+
+btSearch.addEventListener("click", () => {
+  theCity.value = theCity.value.trim();
+  searchWeather();
+});
+
 async function searchWeather() {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    theCity.value +
+    "&appid=" +
+    MY_Api_KEY +
+    "&units=metric&lang=es";
   try {
-    const response = await fetch("");
+    const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("Error en la respuesta ", response.status);
+      throw new Error("Error en la respuesta del servidor: ", response.status);
     }
     const data = await response.json();
+    const temp = data.main.temp;
+    const city = data.name;
+    const desc = data.weather[0].description;
+
+    console.log(temp + " " + city + " " + desc);
   } catch (error) {
-    console.log("Error to response: ", error.message);
+    console.log("Error al conectar con el servidor: ", error.message);
   }
 }
