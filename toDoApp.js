bd38de8a9a7f041613c29ad29deef238583ab2f1
xatoss <xatimatrix@gmail.com>
2026-08-16 19:21:23 +0200

    Add: new funcionality toDo list

---
+++ toDoApp.js
@@ -0,0 +1,10 @@
+const inputTodo = document.querySelector("#inputTodo");
+const btAddTodo = document.querySelector("#btAddTodo");
+const todoList = document.querySelector("#todoList");
+const todoCounter = document.querySelector("#todoCounter");
+
+let lista = new Array
+btAddTodo.addEventListener("click", () => {
+  inputTodo.value = inputTodo.value;
+});
+
